7c723d83b0d40449138b152a38cddf5d00b74e15
Devbrat Kumar <devkumar639575@gmail.com>
2026-01-21 14:15:50 +0530

    [Devbrat kumar] add basic_programs

---
+++ basic_programs/Area_of_Circle.java
@@ -0,0 +1,10 @@
+class Area_of_Circle{
+	public static void main(String args[])
+	{
+		float a=3.14f;
+		int b=100;
+	    float Area=a*b*b;
+     System.out.println("Area of circle:-"+Area);
+		
+	}
+}
