4bbeb772214ab52e20e81c6b3ac2bd511c126d4b
Devbrat Kumar <devkumar639575@gmail.com>
2026-01-29 11:48:48 +0530

    [Devbrat Kumar] commit

---
+++ Arrays/LengthOfArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class LengthOfArray{
+	public static void main(String args[])
+	{
+		int arr[]=new int[6];
+		int arr[0]=10;
+		int arr[1]=15;
+		int arr[2]=20;
+		int arr[3]=25;
+		int arr[4]=30;
+		int arr[5]=35;
+		System.out.println("Print an array");
+		System.out.println(Arrays.toString(arr));
+	}
+}
